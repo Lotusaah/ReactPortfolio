ff1c735bf0cb06bea7b03ac18022682bf1edc1da
victor <vddavis@icloud.com>
2024-06-01 10:10:41 -0400

    added mobile friendly menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,25 +2,24 @@ import "./Footer.css";
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
 import Vlogo from './Vlogo.png';
-
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-gray-800 p-4 width-full">
+    <nav className="bg-gray-800 p-4 w-full z-0">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex"> 
-          <div className="flex-1 flex items-center">
-            <Link to="/">
-              <img src={Vlogo} alt="Logo" className="h-12 text-center vlogo-bg"/>
-            </Link>
-          </div> 
-          <div className="text-white text-lg font-bold flex-1 underline-offset">
+        <div className="flex items-center">
+          <Link to="/">
+            <img src={Vlogo} alt="Logo" className="h-12 text-center vlogo-bg"/>
+          </Link>
+          <div className="text-white text-lg font-bold ml-4 underline-offset">
             <Link to="/">Victor's Portfolio</Link>
           </div>
         </div>
+
         <ul className="space-x-4 hidden md:flex">
-         
           <li>
             <Link to="/" className="text-gray-300 hover:text-white underline-offset">
               About
@@ -37,12 +36,35 @@ export const Navbar = () => {
             </Link>
           </li>
         </ul>
-        <button className="md:hidden text-gray-300 hover:text-white focus:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-      </button>
+
+        <button onClick={() => setIsOpen(!isOpen)} className="md:hidden menu p-2 rounded-lg hover:bg-brand-400 transition-all">
+          <svg className="" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+            <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+          </svg>
+        </button>
       </div>
-     
+
+      {isOpen && (
+        <nav className="md:hidden fixed top-16 right-0 w-full z-10 bg-gray-800 p-4">
+          <ul className="flex flex-col space-y-3">
+            <li>
+              <Link to="/" className="text-gray-300 hover:text-white underline-offset" onClick={() => setIsOpen(false)}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/tictactoe" className="text-gray-300 hover:text-white underline-offset" onClick={() => setIsOpen(false)}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-gray-300 hover:text-white underline-offset" onClick={() => setIsOpen(false)}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </nav>
   );
 };
-
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import "./Home.css";
-import Headshot from '../components/headshot.jpg'
+import Headshot from '../components/headshot.jpg';
 
 export const Home = () => {
   return (
@@ -8,13 +9,13 @@ export const Home = () => {
         <h1 className="p-4vh pb-1vh">Hi. I am.</h1>
         <h1 className="p-4vh pt-0">Victor.</h1>
       </div>
+
       <div className="flex bg-brand-300 pb-4vh">
         <div className="flex-grow bg-brand-300 hidden sm:block lg:flex-grow-1"></div>
         <div className="flex-grow-2 bg-brand-300">
           <div className="bg-white rounded-lg p-10vh">
-            
+            <img src={Headshot} alt="headshot" style={{ height: '10vh' }} />
           </div>
-            <img src={Headshot} alt="headshot" className="h-10vh" />
         </div>
         <div className="flex-grow bg-brand-300 hidden sm:block lg:flex-grow-1"></div>
       </div>
